fd43f288110399344f45d40749b024910e33cd6c
Sidhak Singh <sidhaksingh@Sidhaks-MacBook-Air.local>
2020-06-09 13:30:32 +0530

    Create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error: ${err}`);
+        return;
+    }
+    console.log(`Successfully fired express server at port ${port}`);
+});
